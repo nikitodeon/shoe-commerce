aa987d4e1918d1d7aa457e244729899d41efad70
Nikita <ragerage333@gmail.com>
2025-02-05 16:56:16 +0300

    fix 8

--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { EditForm } from "@/app/components/dashboard/EditForm";
 import prisma from "@/app/lib/db";
 import { notFound } from "next/navigation";
-// import { unstable_noStore as noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
 async function getData(productId: string) {
   const data = await prisma.product.findUnique({
@@ -22,7 +22,7 @@ export default async function EditRoute({
 }: {
   params: Promise<{ id: string }>;
 }) {
-  //   noStore();
+  noStore();
   const { id } = await params;
   const data = await getData(id);
   return <EditForm data={data} />;
